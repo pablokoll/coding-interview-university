d8a84513da3887683c3a0e1024953c90df6bfc93
ideapad-w11-wsl <pablokollm@gmail.com>
2024-07-30 22:21:24 +0200

    first unique character solution

--- practices/mypractices/hashtable/hashmap/leetcode_hashmap_unique-character.ts
+++ practices/mypractices/hashtable/hashmap/leetcode_hashmap_unique-character.ts
@@ -1,22 +1,15 @@
 function firstUniqChar(s: string): number {
     const hashMapChars = new Map<string, number>();
 
-    let uniqueCharIndex = -1;
     for (let i = 0; i < s.length; i++) {
         let count = hashMapChars.get(s[i]) ?? 0;
         count++;
         hashMapChars.set(s[i], count);
     }
 
-    console.log(hashMapChars)
-    
-    let index = 0;
-    for (const value of hashMapChars.values()) {
+    for (const [key, value] of hashMapChars.entries()) {
         if (value === 1) {
-            uniqueCharIndex = index;
-            return uniqueCharIndex;
-        } else {
-            index++;
+            return s.indexOf(key);
         }
     }
     return -1;
